4a352474c4b9fcac28b85301221b3188019fda9a
domie163 <domie1948@163.com>
2025-06-10 16:20:09 +0800

    Initial commit

---
+++ nongchanping/springboot2855f2n2/src/main/resources/front/front/src/config/config.js
@@ -0,0 +1,22 @@
+export default {
+    baseUrl: 'http://localhost:8080/springboot2855f2n2/',
+	name: '/springboot2855f2n2',
+    indexNav: [
+        {
+            name: '产品',
+            url: '/index/zhunongshangpin'
+        },
+        {
+            name: '文章',
+            url: '/index/zhunongtuopin'
+        },
+        {
+            name: '课程',
+            url: '/index/nongjixuetang'
+        },
+        {
+            name: '公告资讯',
+            url: '/index/news'
+        },
+    ]
+}
